3415dc316828aa90357767a339e1a3a5cc7d539a
David <32897194+gloony@users.noreply.github.com>
2018-07-21 20:40:15 +0200

    Don't count spam folder outside mail task

--- unread_favicon.php
+++ unread_favicon.php
@@ -14,17 +14,24 @@
 		}
 		function getunseen(){
 			$rcmail = rcmail::get_instance();
+			$prefs = unserialize($rcmail->user->data['preferences']);
 			$result = 0;
 			$srv = '{'.$_SESSION['storage_host'].':'.$_SESSION['storage_port'].'/imap/'.$_SESSION['storage_ssl'].'}';
 			$mbox=imap_open($srv, $rcmail->user->get_username(), $rcmail->get_user_password(), OP_READONLY);
 			if(!$mbox) die(0);
 			$folders = imap_list($mbox, $srv, '*');
 			foreach($folders as $folder){
-				$items = imap_status($mbox, $folder, SA_UNSEEN);
-				$result += $items->unseen;
+				if(isset($prefs['junk_mbox'])&&!$this->endsWith($folder, $prefs['junk_mbox'])){
+					$items = imap_status($mbox, $folder, SA_UNSEEN);
+					$result += $items->unseen;
+				}
 			}
 			imap_close($mbox);
 			echo $result;
 			exit;
+		}
+		private function endsWith($haystack, $needle){
+			$length = strlen($needle);
+			return $length === 0 || (substr($haystack, -$length) === $needle);
 		}
 	}
